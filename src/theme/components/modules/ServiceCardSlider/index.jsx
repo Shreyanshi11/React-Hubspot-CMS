201190aa226d8cd0b60964c51d7a15e6b16decdb
Sarthak Patel <sarthak.thoughtmate@gmail.com>
2025-06-30 12:27:47 +0530

    Change style

--- src/theme/components/modules/ServiceCardSlider/index.jsx
+++ src/theme/components/modules/ServiceCardSlider/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { logInfo, RichText, Island } from '@hubspot/cms-components';
 import HeadingComponent from '../../components/HeadingComponent/HeadingComponent.jsx';
 import ResponsiveSpacingWrapper from '../../components/SpacingStyleComponent/ResponsiveSpacingWrapper.jsx';
-import Styles from '../ServiceCardSlider/ServiceCard.module.css';
 import ServiceCard from '../../islands/ServiceCardSlider?island';
 
 
@@ -22,7 +21,7 @@ export function Component(props) {
      
         <ResponsiveSpacingWrapper moduleId={props?.module?.module_id} fields={props?.fieldValues}>
           <div>
-            <div className={Styles.inner_slider_container}>
+            <div className='inner_slider_container'>
             <Island module={ServiceCard} groupContent={ groupContent } clientOnly hydrateOn='idle' />
 
             </div>
