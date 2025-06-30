0b1662325572534dd92902a4f3f9cd1380eb16b8
Shreyanshi11 <shreyanshi.thoughtmate@gmail.com>
2025-06-30 15:50:27 +0530

    Commit

--- src/theme/components/modules/FooterSection/index.jsx
+++ src/theme/components/modules/FooterSection/index.jsx
@@ -3,7 +3,7 @@ import { Icon, logInfo, RichText } from '@hubspot/cms-components';
 import FooterStyles from './footer.module.css';
 import RenderImage from '../../components/ImageComponent/imageRenderer.jsx';
 import HeadingComponent from '../../components/HeadingComponent/HeadingComponent.jsx';
-export function Component({ props }) {
+export function Component( props ) {
 
 
     const {
