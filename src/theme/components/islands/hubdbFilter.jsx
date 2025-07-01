5f07e04d17b59b326d3ec8bde1a2980d2d4022bf
dev-sp24 <savan.thoughtmate@gmail.com>
2025-07-01 16:19:50 +0530

    Add Filter

--- src/theme/components/islands/hubdbFilter.jsx
+++ src/theme/components/islands/hubdbFilter.jsx
@@ -2,45 +2,56 @@ import React, { useState } from 'react';
 import Styles from '../../components/modules/HubdbFilter/hubdbFilter.module.css';
 import { logInfo } from '@hubspot/cms-components';
 
-
 export default function HubdbFilter({ language_type, language_image }) {
+  const [selectedType, setSelectedType] = useState(null);
 
-    return (
-        <>
-            <div className={Styles.filter_content}>
-                <div className={Styles.language_name_container}>
-                    <div className={Styles.language_name}>
-                        {/* {language_type.map((item, index) => (
-                            item.map((item2, index) => (
-                                <div className={Styles.language_name_inner} key={index}>
-                                    <p>{item2.name}</p>
-                                </div>
-                            ))
-                        ))} */}
-                        {[
-                            ...new Map(
-                                language_type
-                                    .flat() // Flatten the nested arrays
-                                    .map((item) => [item.label, item]) // Use name as key to filter unique
-                            ).values()
-                        ].map((uniqueItem, index) => (
-                            <div className={Styles.language_name_inner} key={index}>
-                                <p>{uniqueItem.label}</p>
-                            </div>
-                        ))}
-                    </div>
-                </div>
+  // Get unique types
+  const uniqueTypes = [
+    ...new Map(
+      language_type
+        .flat()
+        .map((item) => [item.label, item])
+    ).values()
+  ];
 
-                <div className={Styles.filter_image_container}>
-                    <div className={Styles.filter_image_content}>
-                        {language_image.map((item, index) => (
-                            <div className={Styles.filter_image} key={index}>
-                                <img src={item.url} alt={item.altText} />
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  // Filter images based on selected type
+  const filteredImages = selectedType
+    ? language_image.filter((img) => img.language_type === selectedType)
+    : language_image;
+
+  return (
+    <>
+      <div className={Styles.filter_content}>
+        {/* Filter Buttons */}
+        <div className={Styles.language_name_container}>
+          <div className={Styles.language_name}>
+            {uniqueTypes.map((type, index) => (
+              <div
+                className={`${Styles.language_name_inner} ${
+                  selectedType === type.label ? Styles.active : ''
+                }`}
+                key={index}
+                onClick={() =>
+                  setSelectedType((prev) => (prev === type.label ? null : type.label))
+                }
+              >
+                <p>{type.label}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Image Display */}
+        <div className={Styles.filter_image_container}>
+          <div className={Styles.filter_image_content}>
+            {filteredImages.map((item, index) => (
+              <div className={Styles.filter_image} key={index}>
+                <img src={item.url} alt={item.altText} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
