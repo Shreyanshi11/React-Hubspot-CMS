d77c1d5a6cc84996d3c6df80d1703cd9cbc2de0f
sapnatms <sapna.aghera@thoughtmate.com>
2025-07-02 12:59:37 +0530

    changes on the Recent case studies module

--- src/theme/components/modules/RecentCasestudies/fields.jsx
+++ src/theme/components/modules/RecentCasestudies/fields.jsx
@@ -14,7 +14,7 @@ const buttonFieldVisibility = {
     operator: 'EQUAL',
   }]
 }
-
+const buttonStyleVisibility = buttonFieldVisibility;
 const matchButtonType = {
   controlling_field_path: 'sectionContent.sectionButton.buttonContentType',
   operator: 'MATCHES_REGEX',
@@ -58,7 +58,7 @@ const blockButtonFieldVisibility = {
     operator: 'EQUAL',
   }]
 }
-
+const blockButtonStyleVisibility = blockButtonFieldVisibility;
 const matchBlockButtonType = {
   controlling_field_path: 'imageTextContainer.blockContentContainer.blockButton.buttonContentType',
   operator: 'MATCHES_REGEX',
@@ -94,94 +94,94 @@ const blockIconTypeVisibility = {
 };
 
 export const fields = (
-    <ModuleFields>
-        <FieldGroup tab="STYLE" name="groupStyle" label="Style">
-            <HeadingStyles headingStyleAsDefault="h2" />
-            <FieldGroup name="sectionButton" label="Section Button" display="inline">
-                <ButtonStyle
-                    buttonStyleDefault='simple'
-                    buttonSizeDefault='medium'
-                    buttonSizeVisibility={buttonFieldVisibility}
-                    buttonStyleVisibility={buttonStyleVisibility}
-                />
-            </FieldGroup>
-            <CommonStylesSpacingFields />
-        </FieldGroup>
-        <FieldGroup name="sectionContent" label="Section Content">
-            <HeadingContent />
-            <FieldGroup name="sectionButton" label="Section Button" display="inline">
-                <BooleanField
-                    label='Show button'
-                    name='showButton'
-                    display='toggle'
-                    default={true}
-                />
-                <ButtonContent
-                    textDefault='Read more case studies'
-                    linkDefault={{
-                        open_in_new_tab: false,
-                    }}
-                    iconPositionDefault='left'
-                    buttonVisibility={buttonTypeVisibility}
-                    ctaVisibility={ctaTypeVisibility}
-                    iconVisibility={iconTypeVisibility}
-                />
-            </FieldGroup>
-        </FieldGroup>
+  <ModuleFields>
+    <FieldGroup tab="STYLE" name="groupStyle" label="Style">
+      <HeadingStyles headingStyleAsDefault="h2" />
+      <FieldGroup name="sectionButton" label="Section Button" display="inline">
+        <ButtonStyle
+          buttonStyleDefault='simple'
+          buttonSizeDefault='medium'
+          buttonSizeVisibility={buttonFieldVisibility}
+          buttonStyleVisibility={buttonStyleVisibility}
+        />
+      </FieldGroup>
+      <CommonStylesSpacingFields />
+    </FieldGroup>
+    <FieldGroup name="sectionContent" label="Section Content">
+      <HeadingContent />
+      <FieldGroup name="sectionButton" label="Section Button" display="inline">
+        <BooleanField
+          label='Show button'
+          name='showButton'
+          display='toggle'
+          default={true}
+        />
+        <ButtonContent
+          textDefault='Read more case studies'
+          linkDefault={{
+            open_in_new_tab: false,
+          }}
+          iconPositionDefault='left'
+          buttonVisibility={buttonTypeVisibility}
+          ctaVisibility={ctaTypeVisibility}
+          iconVisibility={iconTypeVisibility}
+        />
+      </FieldGroup>
+    </FieldGroup>
 
-        <RepeatedFieldGroup name="imageTextContainer" label="Image Text Repeated Container">
-            <FieldGroup tab="STYLE" name="blockContentStyle" label="Block Content Style">
-                <ColorField
-                    name="blockBGColor"
-                    label="Block Background color"
-                    required={false}
-                    locked={false}
-                    default={{
-                        color: '#F1F2FF',
-                        opacity: 100,
-                    }}
-                />
-                <HeadingStyles headingStyleAsDefault="h2" />
-                <FieldGroup name="blockButton" label="Block Button" display="inline">
-                    <ButtonStyle
-                        buttonStyleDefault='simple'
-                        buttonSizeDefault='small'
-                        buttonSizeVisibility={blockButtonFieldVisibility}
-                        buttonStyleVisibility={buttonStyleVisibility}
-                    />
-                </FieldGroup>
-            </FieldGroup>
-            <FieldGroup name="blockContentContainer" label="Block Content Container" display="inline">
-                <HeadingContent />
-                <RichTextField name="paragraphText" label="Paragraph Text" />
-                <FieldGroup name="blockButton" label="Block Button" display="inline">
-                    <BooleanField
-                        label='Show button'
-                        name='showButton'
-                        display='toggle'
-                        default={true}
-                    />
-                    <ButtonContent
-                        textDefault='Read More'
-                        linkDefault={{
-                            open_in_new_tab: false,
-                        }}
-                        iconPositionDefault='left'
-                        buttonVisibility={blockButtonTypeVisibility}
-                        ctaVisibility={blockCtaTypeVisibility}
-                        iconVisibility={blockIconTypeVisibility}
-                    />
-                </FieldGroup>
-                <ImageField
-                    label="Image"
-                    name="image"
-                    resizable={true}
-                    responsive={true}
-                    showLoading={true}
-                    visibilityRules="ADVANCED"
-                    advancedVisibility={imageFieldVisibility}
-                />
-            </FieldGroup>
-        </RepeatedFieldGroup>
-    </ModuleFields>
+    <RepeatedFieldGroup name="imageTextContainer" label="Image Text Repeated Container">
+      <FieldGroup tab="STYLE" name="blockContentStyle" label="Block Content Style">
+        <ColorField
+          name="blockBGColor"
+          label="Block Background color"
+          required={false}
+          locked={false}
+          default={{
+            color: '#F1F2FF',
+            opacity: 100,
+          }}
+        />
+        <HeadingStyles headingStyleAsDefault="h2" />
+        <FieldGroup name="blockButton" label="Block Button" display="inline">
+          <ButtonStyle
+            buttonStyleDefault='simple'
+            buttonSizeDefault='small'
+            buttonSizeVisibility={blockButtonFieldVisibility}
+            buttonStyleVisibility={blockButtonStyleVisibility}
+          />
+        </FieldGroup>
+      </FieldGroup>
+      <FieldGroup name="blockContentContainer" label="Block Content Container" display="inline">
+        <HeadingContent />
+        <RichTextField name="paragraphText" label="Paragraph Text" />
+        <FieldGroup name="blockButton" label="Block Button" display="inline">
+          <BooleanField
+            label='Show button'
+            name='showButton'
+            display='toggle'
+            default={true}
+          />
+          <ButtonContent
+            textDefault='Read More'
+            linkDefault={{
+              open_in_new_tab: false,
+            }}
+            iconPositionDefault='left'
+            buttonVisibility={blockButtonTypeVisibility}
+            ctaVisibility={blockCtaTypeVisibility}
+            iconVisibility={blockIconTypeVisibility}
+          />
+        </FieldGroup>
+        <ImageField
+          label="Image"
+          name="image"
+          resizable={true}
+          responsive={true}
+          showLoading={true}
+          visibilityRules="ADVANCED"
+          advancedVisibility={imageFieldVisibility}
+        />
+      </FieldGroup>
+    </RepeatedFieldGroup>
+  </ModuleFields>
 );
