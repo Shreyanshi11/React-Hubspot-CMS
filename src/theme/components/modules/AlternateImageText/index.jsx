b80a73e7b388b5fc4353937247df8c5edf4b4140
sapnatms <sapna.aghera@thoughtmate.com>
2025-07-04 15:40:00 +0530

    Alternate Image Text Module

---
+++ src/theme/components/modules/AlternateImageText/index.jsx
@@ -0,0 +1,79 @@
+import { logInfo, RichText } from "@hubspot/cms-components";
+import Styles from "../AlternateImageText/alternate_image_text.module.css";
+import ResponsiveSpacingWrapper from "../../components/SpacingStyleComponent/ResponsiveSpacingWrapper";
+import HeadingComponent from "../../components/HeadingComponent/HeadingComponent";
+import RenderImage from "../../components/ImageComponent/imageRenderer";
+import { Button } from '../../components/ButtonComponent/ButtonComponent.jsx'
+import {
+    getLinkFieldHref,
+    getLinkFieldRel,
+    getLinkFieldTarget,
+} from '../../utils/content-fields.jsx';
+
+
+export function Component(props) {
+    const {
+        module_id,
+        add_image_text = [],
+    } = props;
+
+    logInfo(props, 'Alternate Image Text');
+    return (
+        <>
+            <ResponsiveSpacingWrapper moduleId={props?.module.module_id} fields={props?.fieldValues}>
+                <div className={Styles.alternate_image_text}>
+                    <div className="page-center">
+                        <div className={Styles.heading_block}>
+                        </div>
+                        <div className={Styles.image_text_container}>
+                            {add_image_text.map((item, index) => (
+                                <div className={Styles.image_text_block}>
+                                    <div class={Styles.image_block}>
+                                        <RenderImage imageField={add_image_text[index]?.image} />
+                                    </div>
+                                    <div className={Styles.text_block}>
+                                        {item.headingAndTextHeading && (
+                                            <div className={Styles.heading_content}>
+                                                <HeadingComponent
+                                                    headingLevel={item.headingAndTextHeadingLevel}
+                                                    headingStyleVariant={item.headingStyleVariant}
+                                                    heading={item.headingAndTextHeading}
+                                                    headingStyleColor={item.headingStyleColor}
+                                                />
+                                            </div>
+                                        )}
+                                        <div className={Styles.paragraph_text}>
+                                            <RichText fieldPath={`add_image_text[${index}].content`} />
+                                        </div>
+
+                                        <div className={Styles.button_content}>
+                                            <Button
+                                                buttonSize={item.buttonStyleSize}
+                                                buttonStyle={item.buttonStyleVariant}
+                                                href={getLinkFieldHref(item.buttonContentLink)}
+                                                rel={getLinkFieldRel(item.buttonContentLink)}
+                                                target={getLinkFieldTarget(item.buttonContentLink)}
+                                                showIcon={item.buttonContentShowIcon}
+                                                iconFieldPath={`add_image_text[${index}].buttonContentIcon`}
+                                                iconPosition={item.buttonContentIconPosition}
+                                                ctaFieldpath={`add_image_text[${index}].ctaField`}
+                                                buttonType={item.buttonContentType}
+                                            >{item.buttonContentText}</Button>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </ResponsiveSpacingWrapper>
+        </>
+    );
+}
+
+export { fields } from './fields.jsx';
+
+
+export const meta = {
+    label: 'Alternate Image Text',
+};
