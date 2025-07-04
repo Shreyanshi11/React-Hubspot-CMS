cb15d0e02beb922fcc3b3d4e1e3ca4aa020305ec
dev-sp24 <savan.thoughtmate@gmail.com>
2025-07-04 15:05:49 +0530

    Change Some Code

--- src/theme/components/modules/HubdbFilter/fields.jsx
+++ src/theme/components/modules/HubdbFilter/fields.jsx
@@ -10,36 +10,20 @@ import HeadingContent from '../../components/HeadingComponent/HeadingContent.jsx
 import RichTextContent from '../../components/fieldLibrary/RichTextField/index.jsx';
 import CommonStylesSpacingFields from '../../components/SpacingStyleComponent/CommonStylesSpacingFields.jsx';
 
-
-
 export const fields = (
     <ModuleFields>
 
-           <BooleanField
-      label='Show Border'
-      name='showBorder'
-      display='toggle'
-      default={false}
-    />
+        <BooleanField label='Show Border' name='showBorder' display='toggle' default={false}/>
 
-
-        <FieldGroup
-            label='Heading Group'
-            name='heading_group'
-        >
+        <FieldGroup label='Heading Group' name='heading_group'>
             <HeadingContent />
             <HeadingStyles headingStyleAsDefault='h2' />
         </FieldGroup>
 
         <HubDbTableField label="Hubdb Table" name="hubdb_table" default="" />
 
-        <FieldGroup
-            label='Styles'
-            name='groupStyle'
-            tab='STYLE'
-        >
+        <FieldGroup label='Styles' name='groupStyle' tab='STYLE'>
             <CommonStylesSpacingFields />
-
         </FieldGroup>
     </ModuleFields>
 );
