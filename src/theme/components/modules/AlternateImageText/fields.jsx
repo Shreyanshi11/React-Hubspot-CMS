d7a40c370a1c499b57acfd72cf67fb8c97fd66b8
Sarthak Patel <sarthak.thoughtmate@gmail.com>
2025-07-07 15:31:04 +0530

    Solve error

--- src/theme/components/modules/AlternateImageText/fields.jsx
+++ src/theme/components/modules/AlternateImageText/fields.jsx
@@ -1,172 +1,164 @@
 import React from "react";
-import { FieldGroup, ModuleFields, RepeatedFieldGroup, ImageField, RichTextField, ChoiceField, BooleanField } from "@hubspot/cms-components/fields";
+import {
+  FieldGroup,
+  ModuleFields,
+  RepeatedFieldGroup,
+  ImageField,
+  RichTextField,
+  ChoiceField,
+  BooleanField,
+} from "@hubspot/cms-components/fields";
+
 import CommonStylesSpacingFields from "../../components/SpacingStyleComponent/CommonStylesSpacingFields";
 import HeadingContent from "../../components/HeadingComponent/HeadingContent";
 import HeadingStyles from "../../components/HeadingComponent/HeadingStyle";
 import ButtonContent from "../../components/ButtonComponent/ButtonContent";
 import ButtonStyle from "../../components/ButtonComponent/ButtonStyle";
 
+// Visibility Conditions
 const sectionButtonFieldVisibility = {
-    boolean_operator: 'OR',
-    criteria: [{
-        controlling_field_path: 'groupButtonContent.showButton',
-        controlling_value_regex: 'true',
-        operator: 'EQUAL',
-    }]
-}
+  boolean_operator: "OR",
+  criteria: [
+    {
+      controlling_field_path: "groupButtonContent.showButton",
+      controlling_value_regex: "true",
+      operator: "EQUAL",
+    },
+  ],
+};
 
 const matchSectionButtonType = {
-    controlling_field_path: 'groupButtonContent.buttonContentType',
-    operator: 'MATCHES_REGEX',
-    controlling_value_regex: '^button$'
+  controlling_field_path: "groupButtonContent.buttonContentType",
+  operator: "MATCHES_REGEX",
+  controlling_value_regex: "^button$",
 };
 
 const matchSectionCtaType = {
-    controlling_field_path: 'groupButtonContent.buttonContentType',
-    operator: 'MATCHES_REGEX',
-    controlling_value_regex: '^cta$'
+  controlling_field_path: "groupButtonContent.buttonContentType",
+  operator: "MATCHES_REGEX",
+  controlling_value_regex: "^cta$",
 };
 
 const sectionButtonTypeVisibility = {
-    boolean_operator: 'OR',
-    criteria: [matchSectionButtonType]
+  boolean_operator: "OR",
+  criteria: [matchSectionButtonType],
 };
 
 const sectionCtaTypeVisibility = {
-    boolean_operator: 'OR',
-    criteria: [matchSectionCtaType]
+  boolean_operator: "OR",
+  criteria: [matchSectionCtaType],
 };
 
 const sectionIconTypeVisibility = {
-    boolean_operator: 'AND',
-    criteria: [
-        matchSectionButtonType,
-        {
-            controlling_field_path: 'groupButtonContent.buttonContentShowIcon',
-            operator: 'MATCHES_REGEX',
-            controlling_value_regex: '^true$'
-        }
-    ]
+  boolean_operator: "AND",
+  criteria: [
+    matchSectionButtonType,
+    {
+      controlling_field_path: "groupButtonContent.buttonContentShowIcon",
+      operator: "MATCHES_REGEX",
+      controlling_value_regex: "^true$",
+    },
+  ],
 };
 
-
 const matchButtonType = {
-    controlling_field_path: 'add_image_text.buttonContentType',
-    operator: 'MATCHES_REGEX',
-    controlling_value_regex: '^button$'
+  controlling_field_path: "add_image_text.buttonContentType",
+  operator: "MATCHES_REGEX",
+  controlling_value_regex: "^button$",
 };
 
 const matchCtaType = {
-    controlling_field_path: 'add_image_text.buttonContentType',
-    operator: 'MATCHES_REGEX',
-    controlling_value_regex: '^cta$'
+  controlling_field_path: "add_image_text.buttonContentType",
+  operator: "MATCHES_REGEX",
+  controlling_value_regex: "^cta$",
 };
 
 const buttonTypeVisibility = {
-    boolean_operator: 'OR',
-    criteria: [matchButtonType]
+  boolean_operator: "OR",
+  criteria: [matchButtonType],
 };
 
 const ctaTypeVisibility = {
-    boolean_operator: 'OR',
-    criteria: [matchCtaType]
+  boolean_operator: "OR",
+  criteria: [matchCtaType],
 };
 
 const iconTypeVisibility = {
-    boolean_operator: 'AND',
-    criteria: [
-        matchButtonType,
-        {
-            controlling_field_path: 'add_image_text.buttonContentShowIcon',
-            operator: 'MATCHES_REGEX',
-            controlling_value_regex: '^true$'
-        }
-    ]
+  boolean_operator: "AND",
+  criteria: [
+    matchButtonType,
+    {
+      controlling_field_path: "add_image_text.buttonContentShowIcon",
+      operator: "MATCHES_REGEX",
+      controlling_value_regex: "^true$",
+    },
+  ],
 };
 
 export const fields = (
-    <ModuleFields>
-        <FieldGroup tab="STYLE" name="groupStyle" label="Style">
-            <HeadingStyles headingStyleAsDefault="h2" />
-            <FieldGroup
-                label='Button Section'
-                name='groupButtonStyle'
-                display='inline'
-            >
-                <ButtonStyle
-                    buttonStyleDefault='primary'
-                    buttonSizeDefault='medium'
-                    buttonSizeVisibility={sectionButtonFieldVisibility}
-                    buttonStyleVisibility={sectionButtonFieldVisibility}
-                />
-            </FieldGroup>
-            <CommonStylesSpacingFields />
-        </FieldGroup>
-        <HeadingContent />
+  <ModuleFields>
+    <FieldGroup tab="STYLE" name="groupStyle" label="Style">
+      <HeadingStyles headingStyleAsDefault="h2" />
+      <FieldGroup label="Button Section" name="groupButtonStyle" display="inline">
+        <ButtonStyle
+          buttonStyleDefault="primary"
+          buttonSizeDefault="medium"
+          buttonSizeVisibility={sectionButtonFieldVisibility}
+          buttonStyleVisibility={sectionButtonFieldVisibility}
+        />
+      </FieldGroup>
+      <CommonStylesSpacingFields />
+    </FieldGroup>
 
-        <FieldGroup
-            label='Button Section'
-            name='groupButtonContent'
-            display='inline'
-        >
-            <BooleanField
-                label='Show button'
-                name='showButton'
-                display='toggle'
-                default={true}
-            />
-            <ButtonContent
-                textDefault='Learn more'
-                linkDefault={{
-                    open_in_new_tab: true,
-                }}
-                iconPositionDefault='left'
-                buttonVisibility={sectionButtonTypeVisibility}
-                ctaVisibility={sectionCtaTypeVisibility}
-                iconVisibility={sectionIconTypeVisibility}
-            />
+    <HeadingContent />
 
-        </FieldGroup>
+    <FieldGroup label="Button Section" name="groupButtonContent" display="inline">
+      <BooleanField label="Show button" name="showButton" display="toggle" default={true} />
+      <ButtonContent
+        namePrefix="group"
+        textDefault="Learn more"
+        linkDefault={{ open_in_new_tab: true }}
+        iconPositionDefault="left"
+        buttonVisibility={sectionButtonTypeVisibility}
+        ctaVisibility={sectionCtaTypeVisibility}
+        iconVisibility={sectionIconTypeVisibility}
+      />
+    </FieldGroup>
 
-
-        <RepeatedFieldGroup name="add_image_text" label="Add Image and Text">
-            <HeadingContent />
-            <HeadingStyles headingStyleAsDefault="h2" />
-            <RichTextField label='Content' name='content' />
-            <ImageField
-                label="Image"
-                name="image"
-                resizable={true}
-                responsive={true}
-                showLoading={true}
-            />
-            <ButtonContent
-                textDefault='Learn more'
-                linkDefault={{
-                    open_in_new_tab: true,
-                }}
-                iconPositionDefault='right'
-                buttonVisibility={buttonTypeVisibility}
-                ctaVisibility={ctaTypeVisibility}
-                iconVisibility={iconTypeVisibility}
-            />
-            <ButtonStyle
-                buttonStyleDefault='primary'
-                buttonSizeDefault='medium'
-            />
-            <ChoiceField
-                name="img_position"
-                label="Image Position"
-                required={false}
-                locked={false}
-                multiple={false}
-                display="select"
-                choices={[
-                    ['img--left', 'Image Left - Text Right'],
-                    ['img--right', 'Text Left - Image Right'],
-                ]}
-                default="img--left"
-            />
-        </RepeatedFieldGroup>
-    </ModuleFields>
+    <RepeatedFieldGroup name="add_image_text" label="Add Image and Text">
+      <HeadingContent />
+      <HeadingStyles headingStyleAsDefault="h2" />
+      <RichTextField label="Content" name="content" />
+      <ImageField
+        label="Image"
+        name="image"
+        resizable={true}
+        responsive={true}
+        showLoading={true}
+      />
+      <ButtonContent
+        namePrefix="repeater"
+        textDefault="Learn more"
+        linkDefault={{ open_in_new_tab: true }}
+        iconPositionDefault="right"
+        buttonVisibility={buttonTypeVisibility}
+        ctaVisibility={ctaTypeVisibility}
+        iconVisibility={iconTypeVisibility}
+      />
+      <ButtonStyle buttonStyleDefault="primary" buttonSizeDefault="medium" />
+      <ChoiceField
+        name="img_position"
+        label="Image Position"
+        required={false}
+        locked={false}
+        multiple={false}
+        display="select"
+        choices={[
+          ["img--left", "Image Left - Text Right"],
+          ["img--right", "Text Left - Image Right"],
+        ]}
+        default="img--left"
+      />
+    </RepeatedFieldGroup>
+  </ModuleFields>
 );
--- src/theme/components/modules/AlternateImageText/index.jsx
+++ src/theme/components/modules/AlternateImageText/index.jsx
@@ -3,119 +3,129 @@ import Styles from "../AlternateImageText/alternate_image_text.module.css";
 import ResponsiveSpacingWrapper from "../../components/SpacingStyleComponent/ResponsiveSpacingWrapper";
 import HeadingComponent from "../../components/HeadingComponent/HeadingComponent";
 import RenderImage from "../../components/ImageComponent/imageRenderer";
-import { Button } from '../../components/ButtonComponent/ButtonComponent.jsx'
+import { Button } from '../../components/ButtonComponent/ButtonComponent.jsx';
 import {
-    getLinkFieldHref,
-    getLinkFieldRel,
-    getLinkFieldTarget,
+  getLinkFieldHref,
+  getLinkFieldRel,
+  getLinkFieldTarget,
 } from '../../utils/content-fields.jsx';
 
-
 export function Component(props) {
-    const {
-        module_id,
-        groupStyle: {
-            headingStyleVariant,
-            headingStyleColor,
-            groupButtonStyle: {
-                buttonStyleSize,
-                buttonStyleVariant,
-            }
-        },
-        headingAndTextHeadingLevel,
-        headingAndTextHeading,
-        groupButtonContent: {
-            showButton,
-            buttonContentText: text,
-            buttonContentLink: link,
-            buttonContentShowIcon: showIcon,
-            buttonContentIconPosition: iconPosition,
-            buttonContentType
-        },
-        add_image_text = [],
-    } = props;
+  const {
+    module_id,
+    groupStyle: {
+      headingStyleVariant,
+      headingStyleColor,
+      groupButtonStyle: {
+        buttonStyleSize,
+        buttonStyleVariant,
+      }
+    },
+    headingAndTextHeadingLevel,
+    headingAndTextHeading,
+    groupButtonContent: {
+      showButton,
+      buttonContentText: text,
+      buttonContentLink: link,
+      buttonContentShowIcon: showIcon,
+      buttonContentIconPosition: iconPosition,
+      buttonContentType
+    },
+    add_image_text = [],
+  } = props;
 
-    logInfo(props, 'Alternate Image Text');
-    return (
-        <>
-            <ResponsiveSpacingWrapper moduleId={props?.module?.module_id} fields={props?.fieldValues}>
-                <div className={Styles.alternate_image_text}>
-                    <div className="page-center">
-                        <div className={Styles.heading_block}>
-                            {headingAndTextHeading && (
-                                <div className={Styles.heading_content}>
-                                    <HeadingComponent
-                                        headingLevel={headingAndTextHeadingLevel}
-                                        headingStyleVariant={headingStyleVariant}
-                                        heading={headingAndTextHeading}
-                                        headingStyleColor={headingStyleColor}
-                                    />
-                                </div>
-                            )}
-                        </div>
-                        <div className={Styles.image_text_container}>
-                            {add_image_text.map((item, index) => (
-                                <div className={`${Styles.image_text_block} ${Styles[item.img_position]}`} key={index}>
-                                    <div className={Styles.image_block}>
-                                        <RenderImage imageField={add_image_text[index]?.image} />
-                                    </div>
-                                    <div className={Styles.text_block}>
-                                        {item.headingAndTextHeading && (
-                                            <div className={Styles.heading_content}>
-                                                <HeadingComponent
-                                                    headingLevel={item.headingAndTextHeadingLevel}
-                                                    headingStyleVariant={item.headingStyleVariant}
-                                                    heading={item.headingAndTextHeading}
-                                                    headingStyleColor={item.headingStyleColor}
-                                                />
-                                            </div>
-                                        )}
-                                        <div className={Styles.paragraph_text}>
-                                            <RichText fieldPath={`add_image_text[${index}].content`} />
-                                        </div>
+  logInfo(props, 'Alternate Image Text');
 
-                                        <div className={Styles.button_content}>
-                                            <Button
-                                                buttonSize={item.buttonStyleSize}
-                                                buttonStyle={item.buttonStyleVariant}
-                                                href={getLinkFieldHref(item.buttonContentLink)}
-                                                rel={getLinkFieldRel(item.buttonContentLink)}
-                                                target={getLinkFieldTarget(item.buttonContentLink)}
-                                                showIcon={item.buttonContentShowIcon}
-                                                iconFieldPath={`add_image_text[${index}].buttonContentIcon`}
-                                                iconPosition={item.buttonContentIconPosition}
-                                                ctaFieldpath={`add_image_text[${index}].ctaField`}
-                                                buttonType={item.buttonContentType}
-                                            >{item.buttonContentText}</Button>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                        <div className={Styles.button_content}>
-                            {showButton && <Button
-                                buttonSize={buttonStyleSize}
-                                buttonStyle={buttonStyleVariant}
-                                href={getLinkFieldHref(link)}
-                                rel={getLinkFieldRel(link)}
-                                target={getLinkFieldTarget(link)}
-                                showIcon={showIcon}
-                                iconFieldPath='groupButtonContent.buttonContentIcon'
-                                iconPosition={iconPosition}
-                                ctaFieldpath={`groupButtonContent.ctaField`}
-                                buttonType={buttonContentType}
-                            >{text}</Button>}
-                        </div>
-                    </div>
+  return (
+    <ResponsiveSpacingWrapper moduleId={props?.module?.module_id} fields={props?.fieldValues}>
+      <div className={Styles.alternate_image_text}>
+        <div className="page-center">
+
+          {/* Section Heading */}
+          {headingAndTextHeading && (
+            <div className={Styles.heading_block}>
+              <div className={Styles.heading_content}>
+                <HeadingComponent
+                  headingLevel={headingAndTextHeadingLevel}
+                  headingStyleVariant={headingStyleVariant}
+                  heading={headingAndTextHeading}
+                  headingStyleColor={headingStyleColor}
+                />
+              </div>
+            </div>
+          )}
+
+          {/* Repeated Blocks */}
+          <div className={Styles.image_text_container}>
+            {add_image_text.map((item, index) => (
+              <div className={`${Styles.image_text_block} ${Styles[item.img_position]}`} key={index}>
+                <div className={Styles.image_block}>
+                  <RenderImage imageField={item.image} />
                 </div>
-            </ResponsiveSpacingWrapper>
-        </>
-    );
+                <div className={Styles.text_block}>
+                  {item.headingAndTextHeading && (
+                    <div className={Styles.heading_content}>
+                      <HeadingComponent
+                        headingLevel={item.headingAndTextHeadingLevel}
+                        headingStyleVariant={item.headingStyleVariant}
+                        heading={item.headingAndTextHeading}
+                        headingStyleColor={item.headingStyleColor}
+                      />
+                    </div>
+                  )}
+                  <div className={Styles.paragraph_text}>
+                    <RichText fieldPath={`add_image_text[${index}].content`} />
+                  </div>
+                  {item.buttonContentText && (
+                    <div className={Styles.button_content}>
+                      <Button
+                        buttonSize={item.buttonStyleSize}
+                        buttonStyle={item.buttonStyleVariant}
+                        href={getLinkFieldHref(item.buttonContentLink)}
+                        rel={getLinkFieldRel(item.buttonContentLink)}
+                        target={getLinkFieldTarget(item.buttonContentLink)}
+                        showIcon={item.buttonContentShowIcon}
+                        iconFieldPath={`add_image_text[${index}].repeater.buttonContentIcon`}
+                        iconPosition={item.buttonContentIconPosition}
+                        ctaFieldpath={`add_image_text[${index}].repeater.ctaField`}
+                        buttonType={item.buttonContentType}
+                      >
+                        {item.buttonContentText}
+                      </Button>
+                    </div>
+                  )}
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {/* Group Button */}
+          {showButton && (
+            <div className={Styles.button_content}>
+              <Button
+                buttonSize={buttonStyleSize}
+                buttonStyle={buttonStyleVariant}
+                href={getLinkFieldHref(link)}
+                rel={getLinkFieldRel(link)}
+                target={getLinkFieldTarget(link)}
+                showIcon={showIcon}
+                iconFieldPath="group.groupButtonContent.buttonContentIcon"
+                iconPosition={iconPosition}
+                ctaFieldpath="group.groupButtonContent.ctaField"
+                buttonType={buttonContentType}
+              >
+                {text}
+              </Button>
+            </div>
+          )}
+        </div>
+      </div>
+    </ResponsiveSpacingWrapper>
+  );
 }
 
 export { fields } from './fields.jsx';
 
-
 export const meta = {
-    label: 'Alternate Image Text',
+  label: 'Alternate Image Text',
 };
