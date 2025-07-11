bf1056188543e5ee27561bdf3532e6f9303f754f
Sarthak Patel <sarthak.thoughtmate@gmail.com>
2025-07-11 17:36:18 +0530

    Change code

--- src/theme/components/modules/Hubdb/index.jsx
+++ src/theme/components/modules/Hubdb/index.jsx
@@ -6,77 +6,80 @@ import HubdbFilter from '../../islands/hubdbFilter?island';
 import Styles from '../HubdbFilter/hubdbFilter.module.css';
 
 export const hublDataTemplate = `
- {% set table1 = hubdb_table_rows(module.hubdb_table) %}
- {% set language_image = [] %}
- {% set language_type = [] %}
- {% for row in table1 %}
-   {% do language_image.append(row.language_image) %}
-   {% do language_type.append(row.language_type) %}
- {% endfor %}
- {% set hublData = {
-   "language_image": language_image,
-   "language_type": language_type,
-   "table1": table1
- } %}
- {{ hublData|tojson }}
+{% set table1 = hubdb_table_rows(module.hubdb_table) %}
+{% set language_image = [] %}
+{% set language_type = [] %}
+{% for row in table1 %}
+{% do language_image.append(row.language_image) %}
+{% do language_type.append(row.language_type) %}
+{% endfor %}
+{% set hublData = {
+"language_image": language_image,
+"language_type": language_type,
+"table1": table1
+} %}
+{{ hublData|tojson }}
 `;
 
 export function Component(props) {
 
-  const image = Array.isArray(props.hublData?.language_image)
-    ? props.hublData.language_image
-    : [];
-  const ltype = Array.isArray(props.hublData?.language_type)
-    ? props.hublData.language_type
-    : [];
+const image = Array.isArray(props.hublData?.language_image)
+? props.hublData.language_image
+: [];
+const ltype = Array.isArray(props.hublData?.language_type)
+? props.hublData.language_type
+: [];
 
-  const {
-    module_id,
-    showBorder,
-    heading_group: {
-      headingAndTextHeadingLevel,
-      headingAndTextHeading,
-      headingStyleVariant
-    },
-    hubdb_table
-  } = props;
+const {
+module_id,
+showBorder,
+heading_group: {
+    headingAndTextHeadingLevel,
+    headingAndTextHeading,
+    headingStyleVariant
+},
+hubdb_table
+} = props;
 
-  logInfo(props, 'Hubdb Module');
+logInfo(props, 'Hubdb Module');
 
-  return (
-    <>
-      <ResponsiveSpacingWrapper moduleId={props?.module?.module_id} fields={props?.fieldValues}>
-        <div className={Styles.hubdb_filter_sec}>
-          <div className="page-center">
-              
+return (
+<>
+    <ResponsiveSpacingWrapper moduleId={props?.module?.module_id} fields={props?.fieldValues}>
+    <div className={Styles.hubdb_filter_sec}>
+        <div className="page-center">
+            
 
-            <div className={Styles.heading_container}>
-              {showBorder && <div className='gradient-border' style={{ margin: '0 auto' , marginBottom: '10px' }}></div>}
-              
-              {headingAndTextHeading && (
-                <HeadingComponent headingLevel={headingAndTextHeadingLevel} headingStyleVariant={headingStyleVariant} heading={headingAndTextHeading} />
-              )}
-            </div>
-
-            <div className={Styles.hubdb_filter_container}>
-              <div className="image-text-container">
-                <div className="image">
-                    <img src={{image}} alt='image' />
-                </div>
-                <div className="text">
-                    <p>{{ltype}}</p>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className={Styles.heading_container}>
+            {showBorder && <div className='gradient-border' style={{ margin: '0 auto' , marginBottom: '10px' }}></div>}
+            
+            {headingAndTextHeading && (
+            <HeadingComponent headingLevel={headingAndTextHeadingLevel} headingStyleVariant={headingStyleVariant} heading={headingAndTextHeading} />
+            )}
         </div>
-      </ResponsiveSpacingWrapper>
-    </>
-  );
+
+        <div className={Styles.hubdb_filter_container}>
+            <div className="image-text-container">
+            <div className="image">
+               
+            </div>
+            <div className="text">
+                    {ltype.map((text, index) => (
+                        <p>{text.label}</p>
+                    ))}
+                
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    </ResponsiveSpacingWrapper>
+</>
+);
 }
 
 export { fields } from './fields.jsx';
 
 export const meta = {
-  label: 'Hubdb Module',
+label: 'Hubdb Module',
 };
